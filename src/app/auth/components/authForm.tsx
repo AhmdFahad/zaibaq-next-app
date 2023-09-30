1cc6eb2d6624d9d4194c8ed48c0492decd357db1
ahamdah <ahamdah@outlook.com>
2023-09-30 22:46:08 +0300

    enhance form desigin

--- src/app/auth/components/authForm.tsx
+++ src/app/auth/components/authForm.tsx
@@ -1,4 +1,11 @@
 "use client";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faEnvelope,
+  faCircleCheck,
+  faKey,
+  faCircleXmark,
+} from "@fortawesome/free-solid-svg-icons";
 import React, { useState } from "react";
 
 interface FormData {
@@ -7,7 +14,7 @@ interface FormData {
 }
 
 const AuthForm = () => {
-  const [isRegistration, setIsRegistration] = useState<boolean>(true);
+  const [isRegistration, setIsRegistration] = useState<boolean>(false);
   const [passwordMatch, setPasswordMatch] = useState("");
   const [formData, setFormData] = useState<FormData>({
     email: "",
@@ -24,6 +31,7 @@ const AuthForm = () => {
   // TODO:HANDLE SUBMIT
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
+    setPasswordMatch("");
 
     if (isRegistration) {
       console.log(
@@ -52,74 +60,113 @@ const AuthForm = () => {
   };
 
   const formTitle = isRegistration ? "انشاء حساب" : "تسجيل الدخول";
+  const passwordInputMatch = passwordMatch === formData.password;
 
   return (
-    <div className="max-w-md mx-auto my-8 p-6 border rounded-lg shadow-lg bg-main-white">
-      <h2 className="text-2xl font-semibold mb-4 text-main-dark border-b-2  border-gray-950 border-opacity-75">
-        {formTitle}
-      </h2>
-      <form onSubmit={handleSubmit} className="text-main-dark">
-        <div className="mb-4">
-          <label className="block ">
-            :الايميل
-            <input
-              className="form-input mt-1 block w-full rounded-md border-gray-300"
-              type="email"
-              name="email"
-              value={formData.email}
-              onChange={handleInputChange}
-              required
-            />
-          </label>
-        </div>
-        <div className="mb-4">
-          <label className="block ">
-            :الرقم السري
-            <input
-              className="form-input mt-1 block w-full rounded-md border-gray-300"
-              type="password"
-              name="password"
-              value={formData.password}
-              onChange={handleInputChange}
-              required
-            />
-          </label>
-        </div>
-        {isRegistration === true ? (
+    <div className="mx-8">
+      <div className="max-w-md mx-auto my-8 p-6 border rounded-lg shadow-lg bg-white">
+        <h2 className="text-2xl font-semibold mb-4 text-main-dark border-b-2  border-gray-950 border-opacity-75">
+          {formTitle}
+        </h2>
+        <form onSubmit={handleSubmit} className="text-main-dark">
           <div className="mb-4">
             <label className="block ">
-              :تاكيد الرقم السري
-              <input
-                className="form-input mt-2 block w-full rounded-md border-gray-300"
-                type="password"
-                name="password-ok"
-                value={passwordMatch}
-                onChange={(e) => setPasswordMatch(e.target.value)}
-                required
-              />
+              <span>:الايميل</span>
+              <div className="relative">
+                <input
+                  className="form-input mt-1 block w-full rounded-md border-b-4 border-main-dark   shadow-sm pr-10 text-center"
+                  type="email"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleInputChange}
+                  required
+                />
+                <FontAwesomeIcon
+                  icon={faEnvelope}
+                  className="absolute right-3 top-1/2 transform -translate-y-1/2 text-main-dark"
+                />
+              </div>
             </label>
           </div>
-        ) : (
-          <></>
-        )}
-        <div>
+          <div className="mb-4">
+            <label className="block ">
+              <span>:الرقم السري</span>
+              <div className="relative">
+                <input
+                  className="form-input mt-1 block w-full rounded-md border-b-4 border-main-dark   shadow-sm pr-10 text-center"
+                  type="password"
+                  name="password"
+                  value={formData.password}
+                  onChange={handleInputChange}
+                  required
+                />
+                <FontAwesomeIcon
+                  icon={faKey}
+                  className="absolute right-3 top-1/2 transform -translate-y-1/2 text-main-dark"
+                />
+              </div>
+            </label>
+          </div>
+          {isRegistration === true ? (
+            <div className="mb-4">
+              <label className="block ">
+                <span>:تاكيد الرقم السري</span>
+                <div className="relative">
+                  <input
+                    className="form-input mt-1 block w-full rounded-md border-b-4 border-main-dark   shadow-sm pr-10 text-center"
+                    type="password"
+                    name="password-ok"
+                    value={passwordMatch}
+                    onChange={(e) => setPasswordMatch(e.target.value)}
+                    required
+                  />
+                  <span
+                    className={
+                      passwordInputMatch ? "text-blue-600" : "text-red-600"
+                    }
+                  >
+                    <FontAwesomeIcon
+                      icon={passwordInputMatch ? faCircleCheck : faCircleXmark}
+                      className="absolute right-3 top-1/2 transform -translate-y-1/2 "
+                    />
+                  </span>
+                </div>
+                {!passwordInputMatch && passwordMatch.length > 0 ? (
+                  <p className="text-sm pt-1 text-red-600 underline">
+                    الرقم السري لايتطابق
+                  </p>
+                ) : (
+                  <></>
+                )}
+              </label>
+            </div>
+          ) : (
+            <></>
+          )}
+          <div>
+            <button
+              disabled={!passwordInputMatch}
+              className={
+                passwordInputMatch
+                  ? "bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-md"
+                  : "bg-blue-950 bg-opacity-60  text-white font-semibold py-2 px-4 rounded-md"
+              }
+              type="submit"
+            >
+              {formTitle}
+            </button>
+          </div>
+        </form>
+        <p className="mt-4 text-main-dark">
           <button
-            className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-md"
-            type="submit"
+            className="text-blue-500 hover:underline ml-2"
+            onClick={toggleForm}
           >
-            {formTitle}
+            {isRegistration ? "سجل الدخول" : "انشئ الأن"}
           </button>
-        </div>
-      </form>
-      <p className="mt-4 text-main-dark">
-        <button
-          className="text-blue-500 hover:underline ml-2"
-          onClick={toggleForm}
-        >
-          {isRegistration ? "سجل الدخول" : "انشئ الأن"}
-        </button>
-        ,{isRegistration ? " لدي حساب" : " ليس لدي حساب"}
-      </p>
+          ,{isRegistration ? " لدي حساب" : " ليس لدي حساب"}
+        </p>
+      </div>
     </div>
   );
 };
