3a7c644f268a836608aeeaf1ace5790aacafa87c
ahamdah <ahamdah@outlook.com>
2023-09-30 17:51:54 +0300

    change footer

--- src/app/components/PrivacyPolicyModal.tsx
+++ src/app/components/PrivacyPolicyModal.tsx
@@ -13,19 +13,22 @@ const PrivacyPolicyModal = () => {
 
   return (
     <div>
-      <button onClick={openModal} className="text-blue-500 underline">
-        Privacy Policy
+      <button
+        onClick={openModal}
+        className="text-gray-300  hover:text-white transition duration-300 pr-2"
+      >
+        سياسة الخصوصيه
       </button>
 
       {isOpen && (
-        <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center text-main-dark">
+        <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center text-main-dark ">
           {/* Background overlay with reduced opacity */}
           <div
             onClick={closeModal}
-            className="fixed top-0 left-0 w-full h-full bg-black transition ease-in-out delay-150 opacity-60 z-10"
+            className="fixed top-0 left-0 w-full h-full bg-black transition ease-in-out delay-150 opacity-60 z-10 "
           ></div>
-          <div className="modal-container relative z-20">
-            <div className="modal-content bg-white rounded-lg p-4 shadow-lg relative">
+          <div className="modal-container relative z-20 mx-5">
+            <div className="modal-content bg-white rounded-lg p-4 shadow-lg relative ">
               <button
                 onClick={closeModal}
                 className="modal-close absolute top-2 left-2 text-gray-500 hover:text-gray-700"
