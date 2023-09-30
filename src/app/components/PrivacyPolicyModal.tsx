d1c4158a1b086a620e5647840c665751116296fe
ahamdah <ahamdah@outlook.com>
2023-09-30 17:39:23 +0300

    add opecitay effect for focus the content

--- src/app/components/PrivacyPolicyModal.tsx
+++ src/app/components/PrivacyPolicyModal.tsx
@@ -19,7 +19,12 @@ const PrivacyPolicyModal = () => {
 
       {isOpen && (
         <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center text-main-dark">
-          <div className="modal-container">
+          {/* Background overlay with reduced opacity */}
+          <div
+            onClick={closeModal}
+            className="fixed top-0 left-0 w-full h-full bg-black transition ease-in-out delay-150 opacity-60 z-10"
+          ></div>
+          <div className="modal-container relative z-20">
             <div className="modal-content bg-white rounded-lg p-4 shadow-lg relative">
               <button
                 onClick={closeModal}
@@ -31,10 +36,14 @@ const PrivacyPolicyModal = () => {
                 سياسة الخصوصية
               </h2>{" "}
               {/* Arabic text example */}
-              <p>
+              <p className="text-right">
                 {/* Your Arabic privacy policy text goes here */}
                 مرحبًا بك في سياسة الخصوصية. يتمثل هدفنا في حماية خصوصيتك
-                ومعلوماتك الشخصية
+                .ومعلوماتك الشخصية
+                <br />
+                <span className="text-red-600 opacity-80">
+                  سيتم توضيح سياسه الخصوصيه قريبا
+                </span>
                 {/* Replace this with your actual Arabic privacy policy content. */}
               </p>
             </div>
